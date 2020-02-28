8484b51066e5c78207612c2d1202c38c302eef0a
Hanson Ng <ng.hanson@hotmail.com>
2020-02-28 18:33:28 -0500

    Actually renames, change maps side for analytics dashboard

---
+++ src/client/components/colour_consts.js
@@ -0,0 +1,13 @@
+//https://www.materialui.co/flatuicolors
+
+export const BLUE_GREY = "#34495e";
+export const DARK_BLUE_GREY = "#2c3e50";
+
+export const LIGHT_BLUE = "#3498db";
+export const BLUE = "#2980b9";
+
+export const LIGHT_RED = "#e74c3c";
+export const RED = "#c0392b";
+
+export const LIGHT_GREEN = "#2ecc71";
+export const GREEN = "#27ae60";
