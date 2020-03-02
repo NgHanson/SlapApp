921c0b672372039cc96943e435dc8ae56ebfd395
Michael Peet <ubernopeavi@gmail.com>
2020-03-02 15:05:37 -0500

    Added example post requests into their own file

---
+++ src/constants/example_post_requests.js
@@ -0,0 +1,74 @@
+const exBody9to5 = {
+     fromYear: '',
+     fromMonth: '',
+     fromDay: 13,
+     fromStartHour: 9,
+     fromStartMin: 0,
+     fromEndHour: 17,
+     fromEndMin: 0,
+     toYear: '',
+     toMonth: '',
+     toDay: 13,
+     toStartHour: 17,
+     toStartMin: 0,
+     toEndHour: 9,
+     toEndMin: 0
+   };
+
+const exBodyWeekday = {
+   fromYear: '',
+   fromMonth: '',
+   fromDay: 13,
+   fromStartHour: 9,
+   fromStartMin: 0,
+   fromEndHour: 17,
+   fromEndMin: 0,
+   toYear: '',
+   toMonth: '',
+   toDay: 15,
+   toStartHour: 9,
+   toStartMin: 0,
+   toEndHour: 17,
+   toEndMin: 0
+};
+
+const exBodyDinner = {
+    fromYear: '',
+    fromMonth: '',
+    fromDay: 14,
+    fromStartHour: 9,
+    fromStartMin: 0,
+    fromEndHour: 17,
+    fromEndMin: 0,
+    toYear: '',
+    toMonth: '',
+    toDay: 14,
+    toStartHour: 17,
+    toStartMin: 0,
+    toEndHour: 21,
+    toEndMin: 0
+};
+
+const exBodyValentines = {
+    fromYear: '',
+    fromMonth: '',
+    fromDay: 14,
+    fromStartHour: 17,
+    fromStartMin: 0,
+    fromEndHour: 21,
+    fromEndMin: 0,
+    toYear: '',
+    toMonth: '',
+    toDay: 7,
+    toStartHour: 17,
+    toStartMin: 0,
+    toEndHour: 21,
+    toEndMin: 0
+};
+
+module.exports = {
+  exBody9to5,
+  exBodyWeekday,
+  exBodyDinner,
+  exBodyValentines
+};
