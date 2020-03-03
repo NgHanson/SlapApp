acd64908f637127a4910b09b59a90eb75d8349aa
Michael Peet <ubernopeavi@gmail.com>
2020-03-03 18:13:17 -0500

    Added new db backup with live pods and new parking lot, added info for post requests from ttn

--- src/server/model/ttn_uplink.js
+++ src/server/model/ttn_uplink.js
@@ -18,3 +18,28 @@
     // [0]     gateways: [ [Object] ]
     // [0]   }
     // [0] }
+    // 
+    // 
+    // 
+// WE WILL BE RECEIVING FOR DEVICE IDS 53, 54, 55
+// ALL OF THESE DEVICES WILL BE IN PARKING LOT 3
+// [0] Received uplink from  53
+// [0] {
+// [0]   app_id: 'slap',
+// [0]   dev_id: '53',
+// [0]   hardware_serial: '0086C456A8F62526',
+// [0]   port: 1,
+// [0]   counter: 9,
+// [0]   payload_raw: <Buffer 01>,
+// [0]   payload_fields: { parkState: 1 },
+// [0]   metadata: {
+// [0]     time: '2020-03-03T23:09:10.982884454Z',
+// [0]     frequency: 914.9,
+// [0]     modulation: 'LORA',
+// [0]     data_rate: 'SF9BW125',
+// [0]     airtime: 164864000,
+// [0]     coding_rate: '4/5',
+// [0]     gateways: [ [Object] ]
+// [0]   }
+// [0] }
+
