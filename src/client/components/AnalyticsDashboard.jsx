7f67e84278bbedadb06772445a57ee143d8f0f5f
Hanson Ng <ng.hanson@hotmail.com>
2020-03-03 19:57:38 -0500

    Styling for dashboard

--- src/client/components/AnalyticsDashboard.jsx
+++ src/client/components/AnalyticsDashboard.jsx
@@ -133,61 +133,80 @@ export default class AnalyticsDashboard extends Component {
   // number of people exceeding limit this week ...
   render() {
     return(
-      <div style={{padding: '40px'}}>
-        <div style={{display: 'flex', alignItems: 'center'}}>
+      <div className={'analytics-dashboard'} style={{ background: Colour.DARK_BLUE_GREY }}>
+        <div style={{display: 'flex', alignItems: 'center', padding: '40px', color: 'white'}}>
           <FaChartBar className="display-icon"/>
           <p style={{margin: '0 0 0 25px'}}>Analytics Dashboard</p>
           <p style={{margin: '0 0 0 25px'}}>{`${lotName}`}</p>
           <p style={{margin: '0 0 0 25px'}}>{`${lotAddress}`}</p>
         </div>
-        <div style={{display: 'flex'}}>
-          <div className={"left-chart-container"} style={{flex: 1}}>
-            <Chart
-              chartType="LineChart"
-              width="100%"
-              height="300px"
-              data={data1}
-              options={options1}
-            />
-            <div className={"metrics-box"} style={{display: 'flex', height: '150px'}}>
-              <div style={{background: Colour.YELLOW, flex: 1, margin: '0 20px 20px 20px', padding: '20px'}}>
-                {`Current Lot Utilization ${95}%`}
+        <div style={{padding: '0 40px 40px 40px', background: Colour.DARK_BLUE_GREY}}>
+          <div style={{display: 'flex'}}>
+            <div className={"left-chart-container"} style={{flex: 1}}>
+              <div style={{margin: '20px'}}>
+                <Chart
+                  chartType="LineChart"
+                  width="100%"
+                  height="300px"
+                  data={data1}
+                  options={options1}
+                />
               </div>
-              <div style={{background: Colour.LIGHT_GREEN, flex: 1, margin: '0 20px 20px 20px', padding: '20px'}}>
-                {`Avg Spot Free Time b/w Parking Events (During Main Hours): ${5} minutes`}
+              <div className={"metrics-box"} style={{display: 'flex', height: '150px'}}>
+                <div style={{background: Colour.YELLOW, flex: 1, margin: '0 20px 20px 20px', padding: '20px'}}>
+                  {`Current Lot Utilization ${95}%`}
+                </div>
+                <div style={{background: Colour.LIGHT_GREEN, flex: 1, margin: '0 20px 20px 20px', padding: '20px'}}>
+                  {`Avg Spot Free Time b/w Parking Events (During Main Hours): ${5} minutes`}
+                </div>
+              </div>
+              <div className={"metrics-box"} style={{display: 'flex', height: '150px'}}>
+                <div style={{background: Colour.LIGHT_RED, flex: 1, margin: '0 20px 20px 20px', padding: '20px'}}>
+                  {`Number of People Exceeding Time Limit Today ${lotTimeViolaters}`}
+                </div>
+                <div style={{background: Colour.LIGHT_GREEN, flex: 1, margin: '0 20px 20px 20px', padding: '20px'}}>
+                  {`Pods Requiring Servicing ${0}`}
+                </div>
+              </div>
+              <div style={{margin: '20px'}}>
+                <Chart
+                  chartType="ColumnChart"
+                  width="100%"
+                  height="300px"
+                  data={data3}
+                  options={options3}
+                />
               </div>
             </div>
-            <div className={"metrics-box"} style={{display: 'flex', height: '150px'}}>
-              <div style={{background: Colour.LIGHT_RED, flex: 1, margin: '0 20px 20px 20px', padding: '20px'}}>
-                {`Number of People Exceeding Time Limit Today ${lotTimeViolaters}`}
+            <div className={"right-chart-container"} style={{flex: 1}}>
+              <div style={{margin: '20px'}}>
+                <Chart
+                  chartType="ColumnChart"
+                  width="100%"
+                  height="300px"
+                  data={data2}
+                  options={options2}
+                />
               </div>
-              <div style={{background: Colour.LIGHT_GREEN, flex: 1, margin: '0 20px 20px 20px', padding: '20px'}}>
-                {`Pods Requiring Servicing ${0}`}
+              <div style={{margin: '20px'}}>
+                <Chart
+                  chartType="PieChart"
+                  data={pieData}
+                  options={pieOptions}
+                  width={"100%"}
+                  height={"300px"}
+                />
+              </div>
+              <div style={{margin: '20px'}}>
+                <Chart
+                  chartType="ColumnChart"
+                  width="100%"
+                  height="300px"
+                  data={data3}
+                  options={options3}
+                />
               </div>
             </div>
-            <Chart
-              chartType="ColumnChart"
-              width="100%"
-              height="300px"
-              data={data3}
-              options={options3}
-            />
-          </div>
-          <div className={"right-chart-container"} style={{flex: 1}}>
-            <Chart
-              chartType="ColumnChart"
-              width="100%"
-              height="300px"
-              data={data2}
-              options={options2}
-            />
-            <Chart
-              chartType="PieChart"
-              data={pieData}
-              options={pieOptions}
-              width={"100%"}
-              height={"300px"}
-            />
           </div>
         </div>
       </div>
